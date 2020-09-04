85f99fd5833442194c50aeab5b0ead1788402a0b
AugustoCiuffoletti <augusto@di.unipi.it>
2020-09-04 18:25:19 +0200

    wrong exercise fixed

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 // Import stylesheets
 import './style.css';
 
-document.getElementById("app").innerHTML = "Primo<br>";
-document.getElementById("app").innerHTML += "Secondo<br>";
-document.getElementById("app").innerHTML += "<b>Terzo</b>";
+window.alert("Hallo world");
