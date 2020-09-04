ea8a85b562dba90ae215a77d47a3a5ce6aa10f5a
AugustoCiuffoletti <augusto@di.unipi.it>
2020-09-04 18:15:55 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-ps6mzm
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-ps6mzm)
---
+++ index.js
@@ -0,0 +1,6 @@
+// Import stylesheets
+import './style.css';
+
+document.getElementById("app").innerHTML = "Primo<br>";
+document.getElementById("app").innerHTML += "Secondo<br>";
+document.getElementById("app").innerHTML += "<b>Terzo</b>";
